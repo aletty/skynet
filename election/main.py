12db74bd0d6e143afbcdfd6f2f59f003701f54b2
patrick <patrickjvarin@gmail.com>
2014-02-23 21:49:27 -0500

    finished main.py

--- election/main.py
+++ election/main.py
@@ -2,14 +2,17 @@ import numpy as np
 import scipy as sp
 
 def sample_surveys(O,B):
-    # FIXME
-    S = zeros(B.shape)
-    for i in S.shape[1]
-        S = np.random.dirichlet(np.exp(B[:,i]))*O
-        return S/np.sum(S)
+    S = []
+
+    for i in range(B.shape[0]):
+        S.append(np.random.dirichlet(np.exp(B[i,:]))*O)
+        S[i] = S[i]/np.sum(S[i])
+
+    return S
 
 def sample_opinion(O,S,W):
-    return np.random.dirichlet(W[0]*O + np.dot(S,W[1:]))
+    avg = W[0]*O + sum([S[i]*w for i, w in enumerate(W[1:])])
+    return np.random.dirichlet(avg)
 
 if __name__ == "__main__":
     T = 100 # number of timesteps
@@ -17,7 +20,7 @@ if __name__ == "__main__":
     M = 5   # number of polls
 
     # Choose biases and weights
-    B = np.random.randn(M,N)
+    B = 100*np.random.randn(M,N)
     W = np.random.dirichlet(np.ones(M+1))
 
     # Generate initial public opinion
@@ -28,7 +31,7 @@ if __name__ == "__main__":
     S = []
 
     for t in range(T):
-        S.append(sample_surveys(O[-1],B))
-        O.append(sample_opinion(O[-1],S[-1],W))
+        S.append(sample_surveys(O[t],B))
+        O.append(sample_opinion(O[t],S[t],W))
 
     print O
