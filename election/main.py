b10defeae8b4968a67936aeee728b3f38dfd2211
patrick <patrickjvarin@gmail.com>
2014-02-25 15:44:15 -0500

    changed the opinion generation model, and modified some priors

--- election/main.py
+++ election/main.py
@@ -6,29 +6,28 @@ def sample_surveys(O,B):
     S = []
 
     for i in range(B.shape[0]):
-        S.append(np.random.dirichlet(np.exp(B[i,:]))*O)
-        S[i] = S[i]/np.sum(S[i])
+        S.append(np.random.dirichlet(np.exp(B[i,:])*O))
 
     return S
 
 def sample_opinion(O,S,W):
-    avg = W[0]*O*300 + 30*sum([S[i]*w for i, w in enumerate(W[1:])])
+    avg = W[0]*O + sum([S[i]*w for i, w in enumerate(W[1:])])
     return np.random.dirichlet(avg)
 
 if __name__ == "__main__":
+    # Define size of network
     T = 100 # number of timesteps
     N = 3   # number of candidates
     M = 5   # number of polls
 
-    # Choose biases and weights
+    # Randomly select biases and weights based on priors
     B = 10*np.random.randn(M,N)
-    weights = np.ones(M+1)
-    weights[0] = 30
-    W = np.random.dirichlet(weights)
+    weights = np.concatenate((np.random.randn(1)+7,np.random.randn(M)))
+    W = np.exp(weights)
 
     # Generate initial public opinion
     O = []
-    O.append(np.random.dirichlet(np.ones(N)))
+    O.append(np.random.dirichlet(2*np.ones(N)))
 
     # Initialize the survey list
     S = []
